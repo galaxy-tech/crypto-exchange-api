d1e6feeab038d741153c56cb383ed732106cae90
Harshad Patil <harshad0402@gmail.com>
2022-07-14 15:08:49 -0700

    API for 1min candles

--- ftx/get_candles.go
+++ ftx/get_candles.go
@@ -23,9 +23,41 @@ type CandleDataResult struct {
 	Result []CandleData	`json:"result"`
 }
 
+const SECONDS_IN_MINUTE = 60
+
 func Query15MinCandles(client *chttp.HTTPClient, market string, count uint64) ([]CandleData, error) {
 
-	var resolution uint64 = 15*60
+	var resolution uint64 = 15 * SECONDS_IN_MINUTE
+	endTime := uint64(time.Now().Unix())
+	startTime := endTime - (count * resolution)
+	url := fmt.Sprintf(
+		"https://ftx.com/api/markets/%s/candles?resolution=%d&start_time=%d&end_time=%d",
+		market, resolution, startTime, endTime)
+
+	if client == nil {
+		client = chttp.NewHTTPClient()
+	}
+	dat, err := client.GetHTTP(url)
+	if err != nil {
+		return nil, errors.Wrap(err, "http client error")
+	}
+
+	cdr := &CandleDataResult{}
+	err = json.Unmarshal(dat, &cdr)
+	if err != nil {
+		return nil, errors.Wrap(err, "json unmarshal error")
+	}
+
+	if not(cdr.Success) {
+		return nil, errors.New("FTX API returned failure")
+	}
+
+	return cdr.Result[:count], nil
+}
+
+func Query1MinCandles(client *chttp.HTTPClient, market string, count uint64) ([]CandleData, error) {
+
+	var resolution uint64 = SECONDS_IN_MINUTE
 	endTime := uint64(time.Now().Unix())
 	startTime := endTime - (count * resolution)
 	url := fmt.Sprintf(
